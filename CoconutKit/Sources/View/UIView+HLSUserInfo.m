2f2a33a15a0f71c9313efa790a490b1b2ce072af
Samuel Défago <defagos@gmail.com>
2011-08-19 08:13:30 +0200

    Not necessary to swizzle dealloc (see http://developer.apple.com/library/ios/#documentation/Cocoa/Conceptual/ObjectiveC/Chapters/ocAssociativeReferences.html)

--- CoconutKit/Sources/View/UIView+HLSUserInfo.m
+++ CoconutKit/Sources/View/UIView+HLSUserInfo.m
@@ -11,30 +11,9 @@
 #import <objc/runtime.h>
 #import "HLSRuntime.h"
 
-// Original implementation of the methods we swizzle
-static void (*s_UIView__dealloc_Imp)(id, SEL) = NULL;
-
 static void *s_userInfoKey = &s_userInfoKey;
 
-@interface UIView (HLSUserInfoPrivate)
-
-- (void)swizzledDealloc;
-
-@end
-
 @implementation UIView (HLSUserInfo)
-
-#pragma mark Class methods
-
-+ (void)initialize
-{
-    // Perform initialization once for the whole inheritance hierarchy
-    if (self != [UIView class]) {
-        return;
-    }
-    
-    s_UIView__dealloc_Imp = (void (*)(id, SEL))HLSSwizzleSelector([UIView class], @selector(dealloc), @selector(swizzledDealloc));
-}
 
 #pragma mark Accessors and mutators
 
@@ -46,19 +25,6 @@ static void *s_userInfoKey = &s_userInfoKey;
 - (void)setUserInfo_hls:(NSDictionary *)userInfo_hls
 {
     objc_setAssociatedObject(self, s_userInfoKey, userInfo_hls, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-}
-
-@end
-
-@implementation UIView (HLSUserInfoPrivate)
-
-#pragma mark Methods injected by swizzling
-
-- (void)swizzledDealloc
-{
-    objc_setAssociatedObject(self, s_userInfoKey, nil, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
-    
-    s_UIView__dealloc_Imp(self, @selector(dealloc));
 }
 
 @end
