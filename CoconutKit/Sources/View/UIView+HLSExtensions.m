3185d1b5543f11c18bffcd72e0d1bd02d5a8e752
Samuel Défago <defagos@gmail.com>
2011-08-24 12:51:40 +0200

    Renamed UIView+HLSUserInfo as UIView+HLSExtensions. Added tag_hls string property to UIView

--- CoconutKit/Sources/View/UIView+HLSExtensions.m
+++ CoconutKit/Sources/View/UIView+HLSExtensions.m
@@ -1,21 +1,32 @@
 //
-//  UIView+HLSUserInfo.m
+//  UIView+HLSExtensions.m
 //  CoconutKit
 //
 //  Created by Samuel Défago on 18.08.11.
 //  Copyright 2011 Hortis. All rights reserved.
 //
 
-#import "UIView+HLSUserInfo.h"
+#import "UIView+HLSExtensions.h"
 
 #import <objc/runtime.h>
 #import "HLSRuntime.h"
 
+static void *s_tagKey = &s_tagKey;
 static void *s_userInfoKey = &s_userInfoKey;
 
-@implementation UIView (HLSUserInfo)
+@implementation UIView (HLSExtensions)
 
 #pragma mark Accessors and mutators
+
+- (NSString *)tag_hls
+{
+    return objc_getAssociatedObject(self, s_tagKey);
+}
+
+- (void)setTag_hls:(NSString *)tag_hls
+{
+    objc_setAssociatedObject(self, s_tagKey, tag_hls, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+}
 
 - (NSDictionary *)userInfo_hls
 {
