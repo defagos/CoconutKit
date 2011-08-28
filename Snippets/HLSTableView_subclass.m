bf495e2e6ded347210af2d6c99586e1ee77fd5de
Samuel Défago <defagos@gmail.com>
2011-08-28 14:33:25 +0200

    Code snippets added

---
+++ Snippets/HLSTableView_subclass.m
@@ -0,0 +1,25 @@
+#pragma mark Object creation and destruction
+
+- (id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier 
+{
+    if ((self = [super initWithStyle:style reuseIdentifier:reuseIdentifier])) {
+        // Code
+    }
+    return self;
+}
+
+- (void)dealloc
+{
+    // Code
+    
+    [super dealloc];
+}
+
+#pragma mark Accessors and mutators
+
+#pragma mark Cell customization
+
+- (void)awakeFromNib
+{
+
+}
---
+++ Snippets/enum.m
@@ -0,0 +1,9 @@
+typedef enum {
+    EnumNameEnumBegin = 0,
+    EnumNameValue = EnumNameEnumBegin,
+    EnumNameValue,
+    EnumNameValue,
+    EnumNameValue,
+    EnumNameEnumEnd,
+    EnumNameEnumSize = EnumNameEnumEnd - EnumNameEnumBegin
+} EnumName;
