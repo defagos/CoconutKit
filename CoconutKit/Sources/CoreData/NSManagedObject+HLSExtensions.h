2233dcc05771b0af1dd1684299d64c6e5f116ccd
Samuel Défago <defagos@gmail.com>
2011-11-19 12:04:37 +0100

    Documentation

--- CoconutKit/Sources/CoreData/NSManagedObject+HLSExtensions.h
+++ CoconutKit/Sources/CoreData/NSManagedObject+HLSExtensions.h
@@ -7,8 +7,8 @@
 //
 
 /**
- * Simply call this macro somewhere in global scope to enable the UIControl injection early, disabling quasi-
- * simultaneous taps. Good places are for example main.m or your application delegate .m file
+ * Simply call this macro somewhere in global scope to enable Core Data validation extensions early. Good places are 
+ * for example main.m or your application delegate .m file
  */
 #define HLSEnableNSManagedObjectValidation()                                                          \
     __attribute__ ((constructor)) void HLSEnableNSManagedObjectValidationConstructor(void)            \
@@ -36,22 +36,42 @@
  */
 @interface NSManagedObject (HLSExtensions)
 
+/**
+ * When called on an NSManagedObject subclass, create a new instance of it (without context parameter, the default
+ * HLSModelManager context is used)
+ */
 + (id)insertIntoManagedObjectContext:(NSManagedObjectContext *)managedObjectContext;
 + (id)insert;
 
+/**
+ * When called on an NSManagedObject subclass, query instances of it matching a predicate, sorting them using the
+ * specified descriptors (without context parameter, the default HLSModelManager context is used)
+ */
 + (NSArray *)filteredObjectsUsingPredicate:(NSPredicate *)predicate
                     sortedUsingDescriptors:(NSArray *)sortDescriptors
                     inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext;
 + (NSArray *)filteredObjectsUsingPredicate:(NSPredicate *)predicate
                     sortedUsingDescriptors:(NSArray *)sortDescriptors;
 
+/**
+ * When called on an NSManagedObject subclass, query all instances of it, sorting them using the specified descriptors
+ * (without context parameter, the default HLSModelManager context is used)
+ */
 + (NSArray *)allObjectsSortedUsingDescriptors:(NSArray *)sortDescriptors
                        inManagedObjectContext:(NSManagedObjectContext *)managedObjectContext;
 + (NSArray *)allObjectsSortedUsingDescriptors:(NSArray *)sortDescriptors;
 
+/**
+ * When called on an NSManagedObject subclass, query all instances of it, without predictable ordering (without context 
+ * parameter, the default HLSModelManager context is used)
+ */
 + (NSArray *)allObjectsInManagedObjectContext:(NSManagedObjectContext *)managedObjectContext;
 + (NSArray *)allObjects;
 
+/**
+ * Inject the improved validation extensions of NSManagedObject (disabled by default). You should not call this method
+ * directly, use the HLSEnableNSManagedObjectValidation macro instead
+ */
 + (void)injectValidation;
 
 - (BOOL)checkValue:(id)value forKey:(NSString *)key error:(NSError **)pError;
