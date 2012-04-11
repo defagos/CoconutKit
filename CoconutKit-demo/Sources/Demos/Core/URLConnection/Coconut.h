cc15fa3213aee9c03b15e91ed23b63f43f680cb9
Samuel Défago <defagos@gmail.com>
2012-04-11 14:08:09 +0200

    Mongoose test web server added

---
+++ CoconutKit-demo/Sources/Demos/Core/URLConnection/Coconut.h
@@ -0,0 +1,13 @@
+//
+//  Coconut.h
+//  CoconutKit-dev
+//
+//  Created by Samuel Défago on 11.04.12.
+//  Copyright (c) 2012 Hortis. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Coconut : NSObject
+
+@end
---
+++ CoconutKit-demo/Sources/Demos/Core/URLConnection/Coconut.m
@@ -0,0 +1,13 @@
+//
+//  Coconut.m
+//  CoconutKit-dev
+//
+//  Created by Samuel Défago on 11.04.12.
+//  Copyright (c) 2012 Hortis. All rights reserved.
+//
+
+#import "Coconut.h"
+
+@implementation Coconut
+
+@end
