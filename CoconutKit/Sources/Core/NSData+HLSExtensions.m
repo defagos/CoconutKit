0cda53adac9a7134d24b3849583c09d45ad91d64
Samuel Défago <defagos@gmail.com>
2012-05-29 18:14:29 +0200

    NSData hash methods now return strings

--- CoconutKit/Sources/Core/NSData+HLSExtensions.m
+++ CoconutKit/Sources/Core/NSData+HLSExtensions.m
@@ -13,54 +13,61 @@
 
 HLSLinkCategory(NSData_HLSExtensions)
 
-static NSData* digest(NSData *data, unsigned char *(*cc_digest)(const void *, CC_LONG, unsigned char *), CC_LONG digestLength)
+static NSString* digest(NSData *data, unsigned char *(*cc_digest)(const void *, CC_LONG, unsigned char *), CC_LONG digestLength)
 {
 	unsigned char md[digestLength];     // C99
     memset(md, 0, sizeof(md));
 	cc_digest([data bytes], [data length], md);
-	return [NSData dataWithBytes:md length:sizeof(md)];
+    
+    // Hexadecimal representation
+    NSMutableString *hexHash = [NSMutableString string];
+    for (NSUInteger i = 0; i < sizeof(md); ++i) {
+        [hexHash appendFormat:@"%02X", md[i]];
+    }
+    
+    return [hexHash lowercaseString];
 }
 
 @implementation NSData (HLSExtensions)
 
 #pragma mark Digest methods
 
-- (NSData *)md2hash
+- (NSString *)md2hash
 {
     return digest(self, CC_MD2, CC_MD2_DIGEST_LENGTH);
 }
 
-- (NSData *)md4hash
+- (NSString *)md4hash
 {
     return digest(self, CC_MD4, CC_MD4_DIGEST_LENGTH);
 }
 
-- (NSData *)md5hash
+- (NSString *)md5hash
 {
     return digest(self, CC_MD5, CC_MD5_DIGEST_LENGTH);
 }
 
-- (NSData *)sha1hash
+- (NSString *)sha1hash
 {
     return digest(self, CC_SHA1, CC_SHA1_DIGEST_LENGTH);
 }
 
-- (NSData *)sha224hash
+- (NSString *)sha224hash
 {
     return digest(self, CC_SHA224, CC_SHA224_DIGEST_LENGTH);
 }
 
-- (NSData *)sha256hash
+- (NSString *)sha256hash
 {
     return digest(self, CC_SHA256, CC_SHA256_DIGEST_LENGTH);
 }
 
-- (NSData *)sha384hash
+- (NSString *)sha384hash
 {
     return digest(self, CC_SHA384, CC_SHA384_DIGEST_LENGTH);
 }
 
-- (NSData *)sha512hash
+- (NSString *)sha512hash
 {
     return digest(self, CC_SHA512, CC_SHA512_DIGEST_LENGTH);
 }
