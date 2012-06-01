cf7665895a4402fcc964debb79af5206519c4b93
Samuel Défago <defagos@gmail.com>
2012-06-01 09:03:21 +0200

    Enriching the label demo

--- CoconutKit-demo/Sources/Demos/View/Label/LabelDemoViewController.h
+++ CoconutKit-demo/Sources/Demos/View/Label/LabelDemoViewController.h
@@ -6,40 +6,49 @@
 //  Copyright (c) 2012 Hortis. All rights reserved.
 //
 
-@interface LabelDemoViewController : HLSViewController <UITextViewDelegate>{
+@interface LabelDemoViewController : HLSViewController <HLSReloadable, UIPickerViewDataSource, UIPickerViewDelegate>{
 @private
     HLSLabel *_label;
-    UILabel *_standardLabel;
+    UILabel *_labelPointSizeLabel;
     
-    UITextView *_textView;
+    UILabel *_standardLabel;
+    UILabel *_standardLabelPointSizeLabel;
+    
+    UIPickerView *_textPickerView;
+    UIPickerView *_fontPickerView;
     
     UISlider *_numberOfLinesSlider;
+    UILabel *_numberOfLinesLabel;
+    
     UISlider *_fontSizeSlider;
-    UISlider *_minFontSizeSlider;
+    UILabel *_fontSizeLabel;
+    
     UISwitch *_adjustsFontSizeToFitWidthSwitch;
+    
+    UISlider *_minFontSizeSlider;
+    UILabel *_minFontSizeLabel;
+    
     UISegmentedControl *_verticalAlignmentSegmentedControl;
     
-    UILabel *_numberOfLinesLabel;
-    UILabel *_fontSizeLabel;
-    UILabel *_minFontSizeLabel;
+    UIPickerView *_lineBreakModePickerView;
 }
 
 @property (nonatomic, retain) IBOutlet HLSLabel *label;
+@property (nonatomic, retain) IBOutlet UILabel *labelPointSizeLabel;
 @property (nonatomic, retain) IBOutlet UILabel *standardLabel;
-
-@property (nonatomic, retain) IBOutlet UITextView *textView;
-
+@property (nonatomic, retain) IBOutlet UILabel *standardLabelPointSizeLabel;
+@property (nonatomic, retain) IBOutlet UIPickerView *textPickerView;
+@property (nonatomic, retain) IBOutlet UIPickerView *fontPickerView;
 @property (nonatomic, retain) IBOutlet UISlider *numberOfLinesSlider;
-@property (nonatomic, retain) IBOutlet UISlider *fontSizeSlider;
-@property (nonatomic, retain) IBOutlet UISlider *minFontSizeSlider;
-@property (nonatomic, retain) IBOutlet UISwitch *adjustsFontSizeToFitWidthSwitch;
-@property (nonatomic, retain) IBOutlet UISegmentedControl *verticalAlignmentSegmentedControl;
-
 @property (nonatomic, retain) IBOutlet UILabel *numberOfLinesLabel;
+@property (nonatomic, retain) IBOutlet UISlider *fontSizeSlider;
 @property (nonatomic, retain) IBOutlet UILabel *fontSizeLabel;
+@property (nonatomic, retain) IBOutlet UISwitch *adjustsFontSizeToFitWidthSwitch;
+@property (nonatomic, retain) IBOutlet UISlider *minFontSizeSlider;
 @property (nonatomic, retain) IBOutlet UILabel *minFontSizeLabel;
+@property (nonatomic, retain) IBOutlet UISegmentedControl *verticalAlignmentSegmentedControl;
+@property (nonatomic, retain) IBOutlet UIPickerView *lineBreakModePickerView;
 
-- (IBAction)updateView:(id)sender;
-- (IBAction)changeFontSize:(id)sender;
+- (IBAction)updateLabels:(id)sender;
 
 @end
--- CoconutKit-demo/Sources/Demos/View/Label/LabelDemoViewController.m
+++ CoconutKit-demo/Sources/Demos/View/Label/LabelDemoViewController.m
@@ -8,14 +8,35 @@
 
 #import "LabelDemoViewController.h"
 
-@interface LabelDemoViewController ()
-
-- (void)updateLabel;
-- (void)updateView;
-
-@end
+static NSArray *s_textExamples = nil;
+static NSArray *s_fontNames = nil;
 
 @implementation LabelDemoViewController
+
+#pragma mark Class methods
+
++ (void)initialize
+{
+    if (self != [LabelDemoViewController class]) {
+        return;
+    }
+    
+    s_textExamples = [[NSArray arrayWithObjects:@"Lorem ipsum",
+                       @"Lorem ipsum dolor sit amet, consetetur sadipscing elitr",
+                       @"At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.",
+                       @"ABCDEFGHIJKLMNOPQRSTUVWXYZ abcdefghijklmnopqrstuvwxyz",
+                       @"--------- -- --------- -------",
+                       @"", 
+                       @"......... ... ......... .... .", nil] retain];
+    
+    NSMutableArray *fontNames = [NSMutableArray array];
+    NSArray *familyNames = [[UIFont familyNames] sortedArrayUsingSelector:@selector(localizedCompare:)];
+    for (NSString *familyName in familyNames) {
+        NSArray *familyFontNames = [[UIFont fontNamesForFamilyName:familyName] sortedArrayUsingSelector:@selector(localizedCompare:)];
+        [fontNames addObjectsFromArray:familyFontNames];
+    }
+    s_fontNames = [[NSArray arrayWithArray:fontNames] retain];
+}
 
 #pragma mark Object creation and destruction
 
@@ -32,41 +53,53 @@
     [super releaseViews];
     
     self.label = nil;
+    self.labelPointSizeLabel = nil;
     self.standardLabel = nil;
-    self.textView = nil;
+    self.standardLabelPointSizeLabel = nil;
+    self.textPickerView = nil;
+    self.fontPickerView = nil;
     self.numberOfLinesSlider = nil;
-    self.fontSizeSlider = nil;
-    self.minFontSizeSlider = nil;
-    self.adjustsFontSizeToFitWidthSwitch = nil;
-    self.verticalAlignmentSegmentedControl = nil;
     self.numberOfLinesLabel = nil;
+    self.fontSizeSlider = nil;
     self.fontSizeLabel = nil;
+    self.adjustsFontSizeToFitWidthSwitch = nil;
+    self.minFontSizeSlider = nil;
     self.minFontSizeLabel = nil;
+    self.verticalAlignmentSegmentedControl = nil;
+    self.lineBreakModePickerView = nil;
 }
 
 #pragma mark Accessors and mutators
 
 @synthesize label = _label;
 
+@synthesize labelPointSizeLabel = _labelPointSizeLabel;
+
 @synthesize standardLabel = _standardLabel;
 
-@synthesize textView = _textView;
+@synthesize standardLabelPointSizeLabel = _standardLabelPointSizeLabel;
+
+@synthesize textPickerView = _textPickerView;
+
+@synthesize fontPickerView = _fontPickerView;
 
 @synthesize numberOfLinesSlider = _numberOfLinesSlider;
 
-@synthesize fontSizeSlider = _fontSizeSlider;
-
-@synthesize minFontSizeSlider = _minFontSizeSlider;
-
-@synthesize adjustsFontSizeToFitWidthSwitch = _adjustsFontSizeToFitWidthSwitch;
-
-@synthesize verticalAlignmentSegmentedControl = _verticalAlignmentSegmentedControl;
-
 @synthesize numberOfLinesLabel = _numberOfLinesLabel;
+
+@synthesize fontSizeSlider = _fontSizeSlider;
 
 @synthesize fontSizeLabel = _fontSizeLabel;
 
+@synthesize adjustsFontSizeToFitWidthSwitch = _adjustsFontSizeToFitWidthSwitch;
+
+@synthesize minFontSizeSlider = _minFontSizeSlider;
+
 @synthesize minFontSizeLabel = _minFontSizeLabel;
+
+@synthesize verticalAlignmentSegmentedControl = _verticalAlignmentSegmentedControl;
+
+@synthesize lineBreakModePickerView = _lineBreakModePickerView;
 
 #pragma mark View lifecycle
 
@@ -74,39 +107,16 @@
 {
     [super viewDidLoad];
     
-    [self updateView];
-    [self updateLabel];
-    self.fontSizeLabel.text = [NSString stringWithFormat:@"%.0f", self.fontSizeSlider.value];    
-}
-
-- (void)updateLabel
-{
-    // HLSLabel
-    self.label.font = [UIFont fontWithName:[self.label.font fontName] size:self.fontSizeSlider.value];
-    self.label.minimumFontSize = self.minFontSizeSlider.value;
-    self.label.numberOfLines = (int)self.numberOfLinesSlider.value;
-    self.label.verticalAlignment = self.verticalAlignmentSegmentedControl.selectedSegmentIndex;
-    self.label.adjustsFontSizeToFitWidth = self.adjustsFontSizeToFitWidthSwitch.on;
-        
-    self.label.text = self.textView.text;
+    self.textPickerView.dataSource = self;
+    self.textPickerView.delegate = self;
     
-    // UILabel
-    self.standardLabel.font = [UIFont fontWithName:[self.label.font fontName] size:self.fontSizeSlider.value];
-    self.standardLabel.minimumFontSize = self.minFontSizeSlider.value;
-    self.standardLabel.numberOfLines = (int)self.numberOfLinesSlider.value;
-    self.standardLabel.adjustsFontSizeToFitWidth = self.adjustsFontSizeToFitWidthSwitch.on;
+    self.fontPickerView.dataSource = self;
+    self.fontPickerView.delegate = self;
     
-    self.standardLabel.text = self.textView.text;
-}
-
-- (void)updateView
-{    
-    self.numberOfLinesSlider.value = self.label.numberOfLines;
-    self.minFontSizeSlider.value = self.label.minimumFontSize;
-    self.adjustsFontSizeToFitWidthSwitch.on = self.label.adjustsFontSizeToFitWidth;
-    self.verticalAlignmentSegmentedControl.selectedSegmentIndex = self.label.verticalAlignment;
-    self.numberOfLinesLabel.text = [NSString stringWithFormat:@"%d", self.label.numberOfLines];
-    self.minFontSizeLabel.text = [NSString stringWithFormat:@"%.0f", self.label.minimumFontSize];
+    self.lineBreakModePickerView.dataSource = self;
+    self.lineBreakModePickerView.delegate = self;
+    
+    [self reloadData];
 }
 
 #pragma mark Orientation management
@@ -128,37 +138,123 @@
     
     self.title = NSLocalizedString(@"Label", @"Label");
     
-    self.textView.text = NSLocalizedString(@"This text is too long to be displayed on 2 lines in a UILabel", 
-                                           @"This text is too long to be displayed on 2 lines in a UILabel");
-    
     [self.verticalAlignmentSegmentedControl setTitle:NSLocalizedString(@"Top", @"Top") forSegmentAtIndex:0];
     [self.verticalAlignmentSegmentedControl setTitle:NSLocalizedString(@"Middle", @"Middle") forSegmentAtIndex:1];
     [self.verticalAlignmentSegmentedControl setTitle:NSLocalizedString(@"Bottom", @"Bottom") forSegmentAtIndex:2];
-    
-    [self updateLabel];
 }
 
-#pragma mark UITextViewDelegate Methods
+#pragma mark HLSReloadable protocol implementation
 
-- (void)textViewDidChange:(UITextView *)textView
+- (void)reloadData
 {
-    [self updateLabel];
-    [self updateView];
+    NSString *text = [s_textExamples objectAtIndex:[self.textPickerView selectedRowInComponent:0]];
+    NSString *fontName = [s_fontNames objectAtIndex:[self.fontPickerView selectedRowInComponent:0]];
+    UILineBreakMode lineBreakMode = [self.lineBreakModePickerView selectedRowInComponent:0];
+        
+    self.label.font = [UIFont fontWithName:fontName size:self.fontSizeSlider.value];
+    self.label.minimumFontSize = self.minFontSizeSlider.value;
+    self.label.numberOfLines = (NSInteger)self.numberOfLinesSlider.value;
+    self.label.verticalAlignment = self.verticalAlignmentSegmentedControl.selectedSegmentIndex;
+    self.label.adjustsFontSizeToFitWidth = self.adjustsFontSizeToFitWidthSwitch.on;
+    self.label.lineBreakMode = lineBreakMode;
+    self.label.text = text;
+    
+    self.standardLabel.font = [UIFont fontWithName:fontName size:self.fontSizeSlider.value];
+    self.standardLabel.minimumFontSize = self.minFontSizeSlider.value;
+    self.standardLabel.numberOfLines = (NSInteger)self.numberOfLinesSlider.value;
+    self.standardLabel.adjustsFontSizeToFitWidth = self.adjustsFontSizeToFitWidthSwitch.on;
+    self.standardLabel.lineBreakMode = lineBreakMode;
+    self.standardLabel.text = text;
+    
+    self.numberOfLinesLabel.text = [NSString stringWithFormat:@"%.0f", self.numberOfLinesSlider.value];
+    self.fontSizeLabel.text = [NSString stringWithFormat:@"%.0f", self.fontSizeSlider.value];
+    self.minFontSizeLabel.text = [NSString stringWithFormat:@"%.0f", self.minFontSizeSlider.value];
+}
+
+#pragma mark UIPickerViewDataSource protocol implementation
+
+- (NSInteger)numberOfComponentsInPickerView:(UIPickerView *)pickerView
+{
+    return 1;
+}
+
+- (NSInteger)pickerView:(UIPickerView *)pickerView numberOfRowsInComponent:(NSInteger)component
+{
+    if (pickerView == self.textPickerView) {
+        return [s_textExamples count];
+    }
+    else if (pickerView == self.fontPickerView) {
+        return [s_fontNames count];
+    }
+    else if (pickerView == self.lineBreakModePickerView) {
+        return UILineBreakModeMiddleTruncation + 1;
+    }
+    else {
+        HLSLoggerError(@"Unknown picker view");
+        return 0;
+    }
+}
+
+#pragma mark UIPickerViewDelegate protocol implementation
+
+- (NSString *)pickerView:(UIPickerView *)pickerView titleForRow:(NSInteger)row forComponent:(NSInteger)component
+{
+    if (pickerView == self.textPickerView) {
+        return [s_textExamples objectAtIndex:row];
+    }
+    else if (pickerView == self.fontPickerView) {
+        return [s_fontNames objectAtIndex:row];
+    }
+    else if (pickerView == self.lineBreakModePickerView) {
+        switch (row) {                
+            case UILineBreakModeCharacterWrap: {
+                return @"UILineBreakModeCharacterWrap";
+                break;
+            }
+                
+            case UILineBreakModeClip: {
+                return @"UILineBreakModeClip";
+                break;
+            }
+                
+            case UILineBreakModeHeadTruncation: {
+                return @"UILineBreakModeHeadTruncation";
+                break;
+            }
+                
+            case UILineBreakModeTailTruncation: {
+                return @"UILineBreakModeTailTruncation";
+                break;
+            }
+                
+            case UILineBreakModeMiddleTruncation: {
+                return @"UILineBreakModeMiddleTruncation";
+                break;
+            }
+                
+            case UILineBreakModeWordWrap:
+            default: {
+                return @"UILineBreakModeWordWrap";
+                break;
+            }
+        }
+    }
+    else {
+        HLSLoggerError(@"Unknown picker view");
+        return nil;
+    }
+}
+
+- (void)pickerView:(UIPickerView *)pickerView didSelectRow:(NSInteger)row inComponent:(NSInteger)component
+{
+    [self reloadData];
 }
 
 #pragma mark Event callbacks
 
-- (IBAction)updateView:(id)sender
+- (IBAction)updateLabels:(id)sender
 {
-    [self updateLabel];
-    [self updateView];
-}
-
-- (IBAction)changeFontSize:(id)sender;
-{
-    [self updateLabel];
-    [self updateView];
-    self.fontSizeLabel.text = [NSString stringWithFormat:@"%.0f", self.fontSizeSlider.value];
+    [self reloadData];
 }
 
 @end
