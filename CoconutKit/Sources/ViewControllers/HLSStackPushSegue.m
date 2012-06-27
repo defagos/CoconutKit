55e36912b179b13d6919a0ad833699effe97ac4a
Samuel Défago <defagos@gmail.com>
2012-06-27 21:16:46 +0200

    Can now push other view controllers into a stack controller using segues

--- CoconutKit/Sources/ViewControllers/HLSStackPushSegue.m
+++ CoconutKit/Sources/ViewControllers/HLSStackPushSegue.m
@@ -17,19 +17,33 @@
 
 - (void)perform
 {
-    if (! [self.sourceViewController isKindOfClass:[HLSStackController class]]) {
-        HLSLoggerError(@"The source view controller is not a stack controller");
-        return;
+    // The source is a stack controller. The segue is used to set its root view controller
+    if ([self.sourceViewController isKindOfClass:[HLSStackController class]]) {
+        HLSStackController *stackController = (HLSStackController *)self.sourceViewController;
+        if (! [self.identifier isEqualToString:@"root"]) {
+            HLSLoggerError(@"The push segue attached to a stack controller must be called 'root'");
+            return;
+        }
+        
+        if ([[stackController viewControllers] count] != 0) {
+            HLSLoggerError(@"The segue called 'root can only be used to set a root view controller. No view controller "
+                           "must have been loaded before");
+            return;
+        }
+        
+        [stackController pushViewController:self.destinationViewController];
     }
-    
-    HLSStackController *stackController = (HLSStackController *)self.sourceViewController;
-    if ([self.identifier isEqualToString:@"root"] && [[stackController viewControllers] count] != 0) {
-        HLSLoggerError(@"The segue called 'root can only be used to set a root view controller. No view controller "
-                       "must have been loaded");
-        return;
+    // The source is an arbitrary view controller. Check that it is embedded into a stack controller, and
+    // push the destination view controller into it
+    else {
+        UIViewController *sourceViewController = (UIViewController *)self.sourceViewController;
+        if (! sourceViewController.stackController) {
+            HLSLoggerError(@"The source view controller is not embedded into a stack controller");
+            return;
+        }
+        
+        [sourceViewController.stackController pushViewController:self.destinationViewController];
     }
-    
-    [stackController pushViewController:self.destinationViewController];
 }
 
 @end
