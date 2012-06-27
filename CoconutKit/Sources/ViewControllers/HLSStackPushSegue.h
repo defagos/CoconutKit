f8d9d6e20355368c0775473958b7efa75deaa2ff
Samuel Défago <defagos@gmail.com>
2012-06-27 22:45:47 +0200

    Documentation

--- CoconutKit/Sources/ViewControllers/HLSStackPushSegue.h
+++ CoconutKit/Sources/ViewControllers/HLSStackPushSegue.h
@@ -8,13 +8,31 @@
 
 #import "HLSTransitionStyle.h"
 
+/**
+ * Segue class for pushing view controllers into an HLSStackController when using storyboards. The source
+ * view controller can implement the -prepareForSegue:sender: method to further customize transition
+ * properties (transition style and duration)
+ *
+ * Each HLSStackController dropped onto a storyboard must be connected to its root view controller using
+ * a segue with the identifier 'root'. To push a view controller B onto another one A already in the stack, 
+ * connect A with B using a HLSStackPushSegue
+ */
 @interface HLSStackPushSegue : UIStoryboardSegue {
 @private
     HLSTransitionStyle m_transitionStyle;
     NSTimeInterval m_duration;
 }
 
+/**
+ * Push animation style
+ * Default value is HLSTransitionStyleNone
+ */
 @property (nonatomic, assign) HLSTransitionStyle transitionStyle;
+
+/**
+ * Push animation duration
+ * Default is kAnimationTransitionDefaultDuration
+ */
 @property (nonatomic, assign) NSTimeInterval duration;
 
 @end
