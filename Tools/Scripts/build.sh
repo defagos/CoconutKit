e0612d32d585ad15fafbf8dccaff32df450e8ce4
Samuel Défago <defagos@gmail.com>
2012-07-05 11:05:45 +0200

    The build script can now be used standalone

--- Tools/Scripts/build.sh
+++ Tools/Scripts/build.sh
@@ -1,6 +1,15 @@
 #!/bin/bash
 
-pushd "${PROJECT_DIR}"/../CoconutKit > /dev/null
-"${PROJECT_DIR}"/../Submodules/make-fmwk/make-fmwk.sh -o /LeStudioSDK/Binaries/CoconutKit -u trunk Release
-"${PROJECT_DIR}"/../Submodules/make-fmwk/make-fmwk.sh -o /LeStudioSDK/Binaries/CoconutKit -u trunk Debug
+env -i bash --noprofile
+
+# Directory containing this file (absolute)
+SCRIPT_FILE_DIR=`dirname $0`
+SCRIPT_FILE_DIR=`cd $SCRIPT_FILE_DIR; pwd`
+
+coconutkit_dir="$SCRIPT_FILE_DIR/../../CoconutKit"
+make_fmwk_dir="$SCRIPT_FILE_DIR/../../Submodules/make-fmwk"
+
+pushd "$coconutkit_dir" > /dev/null
+"$make_fmwk_dir"/make-fmwk.sh -o /LeStudioSDK/Binaries/CoconutKit -u trunk Release
+"$make_fmwk_dir"/make-fmwk.sh -o /LeStudioSDK/Binaries/CoconutKit -u trunk Debug
 popd > /dev/null
