75c59ad6cd27d2a328b44001226ab660bdee8908
Samuel Défago <defagos@gmail.com>
2012-09-07 08:23:38 +0200

    Add test cases for CAMediaTimingFunction extensions

--- CoconutKit/Sources/Core/CAMediaTimingFunction+HLSExtensions.m
+++ CoconutKit/Sources/Core/CAMediaTimingFunction+HLSExtensions.m
@@ -13,9 +13,11 @@
 - (CAMediaTimingFunction *)inverseFunction
 {
     float values1[2];
+    memset(values1, 0, sizeof(values1));
     [self getControlPointAtIndex:1 values:values1];
      
     float values2[2];
+    memset(values2, 0, sizeof(values2));
     [self getControlPointAtIndex:2 values:values2];
     
     // Flip the original curve around the y = 1 - x axis
@@ -27,8 +29,9 @@
 {
     NSMutableString *controlPointsString = [NSMutableString stringWithString:@"["];
     
-    float values[2];
     for (size_t i = 0; i < 4; ++i) {
+        float values[2];
+        memset(values, 0, sizeof(values));
         [self getControlPointAtIndex:i values:values];
         [controlPointsString appendFormat:@"(%.2f, %.2f)", values[0], values[1]];
         
