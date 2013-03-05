29b30e2ad91547c4019423b254d7640c15a38670
Samuel Défago <defagos@gmail.com>
2013-03-05 16:40:19 +0100

    Improve documentation

--- CoconutKit/Sources/Networking/HLSMockDiskConnection.h
+++ CoconutKit/Sources/Networking/HLSMockDiskConnection.h
@@ -11,8 +11,10 @@
 /**
  * A connection managing file URL requests only (creation fails if the URLRequest is not a file URL request). It returns
  * the NSArray of all corresponding file paths as responseObject:
- *   - If the URL is a directory, then the file paths of all files and folders within it are returned
+ *   - If the URL is a directory, then the file paths of all files and folders within it are returned. If the directory
+ *     is empty, an empty array is returned
  *   - If the URL is a file, then its path is returned
+ *   - If the URL does not refer to a valid file, responseObject is nil
  * The duration of the connection is random between 0 and 1 second
  *
  * Two environment variables can be set to simulate connection issues:
