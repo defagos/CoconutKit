1369e451dd786aada239481ee00def270cf4720b
Samuel Défago <defagos@gmail.com>
2013-07-16 14:48:11 +0200

    Make text views scroll automatically

--- CoconutKit-demo/Sources/Demos/View/TextViews/TextViewsDemoViewController.m
+++ CoconutKit-demo/Sources/Demos/View/TextViews/TextViewsDemoViewController.m
@@ -10,6 +10,8 @@
 
 @interface TextViewsDemoViewController ()
 
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+
 @property (nonatomic, weak) IBOutlet HLSTextView *textView1;
 @property (nonatomic, weak) IBOutlet HLSTextView *textView2;
 @property (nonatomic, weak) IBOutlet HLSTextView *textView3;
@@ -24,8 +26,17 @@
 {
     [super viewDidLoad];
     
+    self.scrollView.avoidingKeyboard = YES;
+    
     self.textView1.placeholderText = @"Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda. Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda. Lorem ipsum dolor sit er elit lamet, consectetaur cillium adipisicing pecu, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum. Nam liber te conscient to factor tum poen legum odioque civiuda.";
     self.textView1.placeholderTextColor = [UIColor blueColor];
+}
+
+- (void)viewWillAppear:(BOOL)animated
+{
+    [super viewWillAppear:animated];
+    
+    self.scrollView.contentSize = self.scrollView.bounds.size;
 }
 
 #pragma mark Localization
