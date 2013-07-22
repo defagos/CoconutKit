484f2d7034d4d8013c362e513caba8c8608491f1
Samuel Défago <defagos@gmail.com>
2013-07-22 14:35:21 +0200

    Remove unnecessary methods

--- CoconutKit/Sources/View/HLSTextField.m
+++ CoconutKit/Sources/View/HLSTextField.m
@@ -8,13 +8,7 @@
 
 #import "HLSTextField.h"
 
-#import "HLSFloat.h"
-#import "HLSLogger.h"
 #import "HLSTextFieldTouchDetector.h"
-
-// The minimal distance to be kept between the active text field and the top of the scroll view top or the keyboard. If the
-// scroll view area is too small to fulfill both, visibility at the top wins
-const CGFloat kTextFieldMinVisibilityDistance = 20.f;           // Corresponds to IB guides
 
 @interface HLSTextField ()
 
@@ -45,8 +39,6 @@ const CGFloat kTextFieldMinVisibilityDistance = 20.f;           // Corresponds t
 // Common initialization code
 - (void)hlsTextFieldInit
 {
-    self.minVisibilityDistance = kTextFieldMinVisibilityDistance;
-    
     self.touchDetector = [[[HLSTextFieldTouchDetector alloc] initWithTextField:self] autorelease];
     super.delegate = self.touchDetector;
 }
@@ -55,24 +47,10 @@ const CGFloat kTextFieldMinVisibilityDistance = 20.f;           // Corresponds t
 {
     self.touchDetector = nil;
     
-    [[NSNotificationCenter defaultCenter] removeObserver:self];
-    
     [super dealloc];
 }
 
 #pragma mark Accessors and mutators
-
-- (void)setTextFieldMinVisibilityDistance:(CGFloat)minVisibilityDistance
-{
-    // Sanitize input
-    if (floatlt(minVisibilityDistance, 0.f)) {
-        HLSLoggerWarn(@"Invalid value; must be positive");
-        _minVisibilityDistance = 0.f;
-    }
-    else {
-        _minVisibilityDistance = minVisibilityDistance;
-    }
-}
 
 - (BOOL)resigningFirstResponderOnTap
 {
