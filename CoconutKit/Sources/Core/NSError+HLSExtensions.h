763598f2ade84daa175f5e2b56ea61b7841417c3
Samuel Défago <defagos@gmail.com>
2014-04-10 08:11:18 +0200

    Turn HLSError into NSError extensions

--- CoconutKit/Sources/Core/NSError+HLSExtensions.h
+++ CoconutKit/Sources/Core/NSError+HLSExtensions.h
@@ -6,7 +6,34 @@
 //  Copyright (c) 2011 Hortis. All rights reserved.
 //
 
+/**
+ * Additions to NSError providing a convenient way to create errors and to define and access their properties.
+ *
+ * The interface below enforces the link between accessor methods and underlying userInfo dictionary keys. This is not
+ * required, as explained in the documentation:
+ *   http://developer.apple.com/library/ios/#documentation/Cocoa/Conceptual/ErrorHandlingCocoa/ErrorHandling/ErrorHandling.html
+ *
+ * Designated initializer: -initWithDomain:code:userInfo:
+ */
 @interface NSError (HLSExtensions)
+
+/**
+ * Instantiate an error with some code within a domain. The error is created with no information, use the mutators below to
+ * add the information you need
+ */
++ (id)errorWithDomain:(NSString *)domain code:(NSInteger)code;
+
+/**
+ * Convenience instantiation method for the most common case (an error conveying a description message). You can still
+ * use the mutators below to add more information if needed
+ */
++ (id)errorWithDomain:(NSString *)domain code:(NSInteger)code localizedDescription:(NSString *)localizedDescription;
+
+/**
+ * Initialize an error with some code within a domain. The error is created with no information, use the mutators below to
+ * add the information you need
+ */
+- (id)initWithDomain:(NSString *)domain code:(NSInteger)code;
 
 /**
  * Return the nested error which has been set (if any)
@@ -14,14 +41,9 @@
 - (NSError *)underlyingError;
 
 /**
- * Return the errors embedded into the error
- */
-- (NSArray *)errors;
-
-/**
  * Return the object set for a given key. The key can either be a reserved one (see NSError) or a custom
  * one. Instead of using this generic accessor to retrieve objects corresponding to reserved keys, use the
- * standard accessors provided by NSError and HLSError
+ * standard accessors provided by NSError, or the additional ones provided above
  */
 - (id)objectForKey:(NSString *)key;
 
@@ -29,6 +51,28 @@
  * Return the user-specific information (i.e. without the information corresponding to reserved keys)
  */
 - (NSDictionary *)customUserInfo;
+
+/**
+ * Various mutators for setting standard NSError properties. Please refer to the NSError documentation for more information
+ */
+- (void)setLocalizedDescription:(NSString *)localizedDescription;
+- (void)setLocalizedFailureReason:(NSString *)localizedFailureReason;
+- (void)setLocalizedRecoverySuggestion:(NSString *)localizedRecoverySuggestion;
+- (void)setLocalizedRecoveryOptions:(NSArray *)localizedRecoveryOptions;
+- (void)setRecoveryAttempter:(id)recoveryAttempter;
+- (void)setHelpAnchor:(NSString *)helpAnchor;
+
+/**
+ * Set a nested error
+ */
+- (void)setUnderlyingError:(NSError *)underlyingError;
+
+/**
+ * Sets an object for some key. The key can either be a reserved one (see NSError header) or a custom one. Instead
+ * of using this generic mutator to set objects corresponding to reserved keys, you should use the mutators 
+ * provided above
+ */
+- (void)setObject:(id)object forKey:(NSString *)key;
 
 /**
  * Return YES iff the receiver has the provided error domain and code
--- CoconutKit/Sources/Core/NSError+HLSExtensions.m
+++ CoconutKit/Sources/Core/NSError+HLSExtensions.m
@@ -8,27 +8,45 @@
 
 #import "NSError+HLSExtensions.h"
 
+#import "HLSAssert.h"
 #import "HLSLogger.h"
+#import "NSDictionary+HLSExtensions.h"
+
+// TODO: Test creation, mutability and copy (copy should work out of the box)
+
+static void *s_mutableUserInfoKey = &s_mutableUserInfoKey;
+
+// Dynamic subclass method implementations
+static NSDictionary *subclass_userInfo(id self, SEL _cmd);
+static Class subclass_class(id self, SEL _cmd);
 
 @implementation NSError (HLSExtensions)
+
+#pragma mark Class methods
+
++ (id)errorWithDomain:(NSString *)domain code:(NSInteger)code
+{
+    return [[[self class] alloc] initWithDomain:domain code:code];
+}
+
++ (id)errorWithDomain:(NSString *)domain code:(NSInteger)code localizedDescription:(NSString *)localizedDescription
+{
+    NSDictionary *userInfo = localizedDescription ? @{ NSLocalizedDescriptionKey : localizedDescription} : nil;
+    return [[[self class] alloc] initWithDomain:domain code:code userInfo:userInfo];
+}
+
+#pragma mark Object creation and destruction
+
+- (id)initWithDomain:(NSString *)domain code:(NSInteger)code
+{
+    return [self initWithDomain:domain code:code userInfo:nil];
+}
 
 #pragma mark Accessors and mutators
 
 - (NSError *)underlyingError
 {
     return [self objectForKey:NSUnderlyingErrorKey];
-}
-
-- (NSArray *)errors
-{
-    // At most one error can be stored in an NSError using the standard NSUnderlyingErrorKey key
-    NSError *error = [[self userInfo] objectForKey:NSUnderlyingErrorKey];
-    if (error) {
-        return [NSArray arrayWithObject:error];
-    }
-    else {
-        return nil;
-    }
 }
 
 - (id)objectForKey:(NSString *)key
@@ -54,6 +72,100 @@
     return [NSDictionary dictionaryWithDictionary:customUserInfo];
 }
 
+- (NSMutableDictionary *)mutableUserInfo
+{
+    NSMutableDictionary *mutableUserInfo = objc_getAssociatedObject(self, s_mutableUserInfoKey);
+    if (! mutableUserInfo) {
+        mutableUserInfo = [NSMutableDictionary dictionaryWithDictionary:self.userInfo];
+        
+        // NSError is immutable, but this makes it rather inconvenient to use (you must set all information
+        // as a dictionary when the error is created). This category adds a set of mutators which let you
+        // set error information after creation (this information should not be altered anymore after the
+        // error has been returned to the caller, of course). This behavior requires an additional mutable
+        // dictionary, which we add by dynamic subclassing only if a mutator is used
+        static NSString * const kSubclassSuffix = @"_HLSExtensions";
+        
+        // Access the real class, do not use [self class] here since can be faked
+        Class class = object_getClass(self);
+        NSString *className = [NSString stringWithUTF8String:class_getName(class)];
+        if (! [className hasSuffix:kSubclassSuffix]) {
+            NSString *subclassName = [className stringByAppendingString:kSubclassSuffix];
+            Class subclass = NSClassFromString(subclassName);
+            if (! subclass) {
+                subclass = objc_allocateClassPair(class, [subclassName UTF8String], 0);
+                NSAssert(subclass != Nil, @"Could not register subclass");
+                class_addMethod(subclass,
+                                @selector(userInfo),
+                                (IMP)subclass_userInfo,
+                                method_getTypeEncoding(class_getClassMethod(class, @selector(userInfo))));
+                class_addMethod(subclass,
+                                @selector(class),
+                                (IMP)subclass_class,
+                                method_getTypeEncoding(class_getClassMethod(class, @selector(class))));
+                objc_registerClassPair(subclass);
+            }
+            
+            // Changes the object class
+            object_setClass(self, subclass);
+        }
+        
+        objc_setAssociatedObject(self, s_mutableUserInfoKey, mutableUserInfo, OBJC_ASSOCIATION_RETAIN_NONATOMIC);
+    }
+    return mutableUserInfo;
+}
+
+- (void)setLocalizedDescription:(NSString *)localizedDescription
+{
+    [self setObject:localizedDescription forKey:NSLocalizedDescriptionKey];
+}
+
+- (void)setLocalizedFailureReason:(NSString *)localizedFailureReason
+{
+    [self setObject:localizedFailureReason forKey:NSLocalizedFailureReasonErrorKey];
+}
+
+- (void)setLocalizedRecoverySuggestion:(NSString *)localizedRecoverySuggestion
+{
+    [self setObject:localizedRecoverySuggestion forKey:NSLocalizedRecoverySuggestionErrorKey];
+}
+
+- (void)setLocalizedRecoveryOptions:(NSArray *)localizedRecoveryOptions
+{
+    HLSAssertObjectsInEnumerationAreKindOfClass(localizedRecoveryOptions, NSString);
+    
+    [self setObject:localizedRecoveryOptions forKey:NSLocalizedRecoveryOptionsErrorKey];
+}
+
+- (void)setRecoveryAttempter:(id)recoveryAttempter
+{
+    [self setObject:recoveryAttempter forKey:NSRecoveryAttempterErrorKey];
+}
+
+- (void)setHelpAnchor:(NSString *)helpAnchor
+{
+    [self setObject:helpAnchor forKey:NSHelpAnchorErrorKey];
+}
+
+- (void)setUnderlyingError:(NSError *)underlyingError
+{
+    [self setObject:underlyingError forKey:NSUnderlyingErrorKey];
+}
+
+- (void)setObject:(id)object forKey:(NSString *)key
+{
+    if (! key) {
+        HLSLoggerError(@"Missing key");
+        return;
+    }
+    
+    if (object) {
+        [[self mutableUserInfo] setObject:object forKey:key];
+    }
+    else {
+        [[self mutableUserInfo] removeObjectForKey:key];
+    }
+}
+
 #pragma mark Comparison
 
 - (BOOL)hasCode:(NSInteger)code withinDomain:(NSString *)domain
@@ -62,3 +174,17 @@
 }
 
 @end
+
+#pragma mark Dynamic subclass method implementations
+
+static NSDictionary *subclass_userInfo(NSError *self, SEL _cmd)
+{
+    return [NSDictionary dictionaryWithDictionary:[self mutableUserInfo]];
+}
+
+static Class subclass_class(NSError *self, SEL _cmd)
+{
+    // Lie about the dynamic subclass existence, as the KVO implementation does (the real class can still be seen
+    // using object_getClass)
+    return class_getSuperclass(object_getClass(self));
+}
