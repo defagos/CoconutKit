dfcebf3dc692798e953a021a9e10d088d71262c2
Samuel Défago <defagos@gmail.com>
2014-09-30 15:47:48 +0200

    Remove useless conversion methods

--- CoconutKit/Sources/Core/HLSConverters.h
+++ CoconutKit/Sources/Core/HLSConverters.h
@@ -18,30 +18,3 @@ NSString *HLSStringFromCATransform3D(CATransform3D transform);
  * Conversions to numbers
  */
 NSNumber *HLSUnsignedIntNumberFromString(NSString *string);
-
-/**
- * Conversions requiring several arguments. As methods since method signatures more explicit
- *
- * Not meant to be instantiated
- */
-@interface HLSConverters : NSObject
-
-+ (NSDate *)dateFromString:(NSString *)string usingFormatString:(NSString *)formatString;
-
-+ (void)convertStringValueForKey:(NSString *)sourceKey 
-                    ofDictionary:(NSDictionary *)sourceDictionary
-           intoStringValueForKey:(NSString *)destKey 
-                    ofDictionary:(NSMutableDictionary *)destDictionary;
-
-+ (void)convertStringValueForKey:(NSString *)sourceKey 
-                    ofDictionary:(NSDictionary *)sourceDictionary
-      intoUnsignedIntValueForKey:(NSString *)destKey 
-                    ofDictionary:(NSMutableDictionary *)destDictionary;
-
-+ (void)convertStringValueForKey:(NSString *)sourceKey  
-                    ofDictionary:(NSDictionary *)sourceDictionary
-             intoDateValueForKey:(NSString *)destKey
-                    ofDictionary:(NSMutableDictionary *)destDictionary
-               usingFormatString:(NSString *)formatString;
-
-@end
--- CoconutKit/Sources/Core/HLSConverters.m
+++ CoconutKit/Sources/Core/HLSConverters.m
@@ -8,7 +8,6 @@
 
 #import "HLSConverters.h"
 
-#import "HLSAssert.h"
 #import "HLSLogger.h"
 
 NSString *HLSStringFromBool(BOOL yesOrNo)
@@ -102,69 +101,3 @@ NSNumber *HLSUnsignedIntNumberFromString(NSString *string)
     [formatter setNumberStyle:NSNumberFormatterDecimalStyle];
     return [formatter numberFromString:string];
 }
-
-@implementation HLSConverters
-
-#pragma mark Class methods
-
-+ (NSDate *)dateFromString:(NSString *)string usingFormatString:(NSString *)formatString
-{
-    if (! string) {
-        return nil;
-    }
-    
-    NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
-    [formatter setFormatterBehavior:NSDateFormatterBehavior10_4];
-    [formatter setDateFormat:formatString];
-    return [formatter dateFromString:string];
-}
-
-+ (void)convertStringValueForKey:(NSString *)sourceKey 
-                    ofDictionary:(NSDictionary *)sourceDictionary
-           intoStringValueForKey:(NSString *)destKey 
-                    ofDictionary:(NSMutableDictionary *)destDictionary
-{
-    NSString *stringValue = [sourceDictionary objectForKey:sourceKey];
-    if (stringValue) {
-        [destDictionary setObject:stringValue forKey:destKey];
-    }
-}
-
-+ (void)convertStringValueForKey:(NSString *)sourceKey 
-                    ofDictionary:(NSDictionary *)sourceDictionary
-      intoUnsignedIntValueForKey:(NSString *)destKey 
-                    ofDictionary:(NSMutableDictionary *)destDictionary
-{
-    NSString *stringValue = [sourceDictionary objectForKey:sourceKey];
-    if (stringValue) {
-        NSNumber *number = HLSUnsignedIntNumberFromString(stringValue);
-        if (number) {
-            [destDictionary setObject:number forKey:destKey];
-        }
-    }
-}
-
-+ (void)convertStringValueForKey:(NSString *)sourceKey  
-                    ofDictionary:(NSDictionary *)sourceDictionary
-             intoDateValueForKey:(NSString *)destKey
-                    ofDictionary:(NSMutableDictionary *)destDictionary
-               usingFormatString:(NSString *)formatString
-{
-    NSString *stringValue = [sourceDictionary objectForKey:sourceKey];
-    if (stringValue) {
-        NSDate *date = [HLSConverters dateFromString:stringValue usingFormatString:formatString];
-        if (date) {
-            [destDictionary setObject:date forKey:destKey];
-        }
-    }
-}
-
-#pragma mark Object creation and destruction
-
-- (instancetype)init
-{
-    HLSForbiddenInheritedMethod();
-    return nil;
-}
-
-@end
