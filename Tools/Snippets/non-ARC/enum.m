90f6cd58dbcc62b64c6a02b2f0801b443c2c45ba
Samuel Défago <defagos@gmail.com>
2014-10-01 13:16:05 +0200

    Remove useless snippets

--- Tools/Snippets/non-ARC/enum.m
+++
@@ -1,9 +0,0 @@
-typedef enum {
-    EnumNameEnumBegin = 0,
-    EnumNameValue = EnumNameEnumBegin,
-    EnumNameValue,
-    EnumNameValue,
-    EnumNameValue,
-    EnumNameEnumEnd,
-    EnumNameEnumSize = EnumNameEnumEnd - EnumNameEnumBegin
-} EnumName;
