d8e1d5b492d80c3509a0359779774fc293555d5a
Samuel Défago <defagos@gmail.com>
2014-10-27 16:26:22 +0100

    Remove keyboard fixes
    Found more bugs which I could not reliably fix. Better wait until the bugs are fixed

--- CoconutKit/Sources/Core/HLSKeyboardInformation.m
+++ CoconutKit/Sources/Core/HLSKeyboardInformation.m
@@ -11,6 +11,17 @@
 #import "HLSLogger.h"
 #import "HLSRuntime.h"
 
+// FIXME: There are serious bugs with the undocked keyboard in iOS 8 / 8.1:
+//          - when undocked by dragging the keyboard upwards, the keyboard does not transition to undocked state. The
+//            hide notifications are not received and, if you long-press the keyboard button, the keyboard can still
+//            be undocked. The behavior is correct when the keyboard is undocked by long-pressing the keyboard button
+//            and tapping Undock. See http://openradar.appspot.com/18010127.
+//          - conversely, when the undocked keyboard is thrown against the bottom, depending on the velocity, show
+//            notifications might be received or not
+//          - when moving the undocked the keyboard to the top, the keyboard jumps everywhere like mad
+//          - the keyboard background has a really weird behavior, sometimes appears and never disappears, sometimes
+//            gets bigger and then suddenly smaller, etc.
+
 @interface HLSKeyboardInformation ()
 
 @property (nonatomic, assign) CGRect beginFrame;
@@ -131,83 +142,3 @@ __attribute__ ((constructor)) static void HLSKeyboardInformationInit(void)
 #endif
 
 }
-
-// FIXME: There is a bug with keyboard undocking in iOS 8 / 8.1 (the UIKeyboardWill/DidHideNotification are not sent correctly
-//        when the keyboard is undocked). See http://openradar.appspot.com/18010127. Remove all code below when this has been
-//        fixed and CoconutKit minimum required OS version includes the bug fix
-//
-//
-//        Analysis:
-//
-//        -postStartNotifications:withInfo: is called with value = 1 when the keyboard is moved, whether it is docked or not. Sadly,
-//        the result is therefore the same in both cases: Only the frame change notification is sent, the hide notification is
-//        therefore not received if the keyboard gets undocked as a result of the keyboard being moved.
-//
-//        When undocking the keyboard by long pressing the keyboard button, the -postStartNotifications:withInfo: is called with
-//        value = 3 and both notifications are correctly received.
-//
-//        To fix this annoying bug until Apple takes care of it, we therefore check whether the keyboard is docked when it is
-//        moved, and in this case call the method with value = 3 instead
-//
-
-typedef NS_ENUM(NSInteger, UIInputWindowNotificationMode) {
-    UIInputWindowNotificationModeMoving = 1,
-    UIInputWindowNotificationModeDocking = 2,
-    UIInputWindowNotificationModeUndocking = 3
-};
-
-static void (*s_UIInputWindowController__postStartNotifications_withInfo_Imp)(id, SEL, UIInputWindowNotificationMode, id) = NULL;
-static void (*s_UIInputWindowController__postEndNotifications_withInfo_Imp)(id, SEL, UIInputWindowNotificationMode, id) = NULL;
-
-@protocol HLSUIInputWindowController <NSObject>
-
-- (BOOL)isUndocked;
-
-@end
-
-static void swizzled_UIInputWindowController__postStartNotifications_withInfo(id<HLSUIInputWindowController> self,
-                                                                              SEL _cmd,
-                                                                              UIInputWindowNotificationMode mode,
-                                                                              id info /* UIInputViewSetNotificationInfo */)
-{
-    if (mode == UIInputWindowNotificationModeMoving && ! [self isUndocked]) {
-        (*s_UIInputWindowController__postStartNotifications_withInfo_Imp)(self, _cmd, UIInputWindowNotificationModeUndocking, info);
-    }
-    else {
-        (*s_UIInputWindowController__postStartNotifications_withInfo_Imp)(self, _cmd, mode, info);
-    }
-}
-
-static void swizzled_UIInputWindowController__postEndNotifications_withInfo(id<HLSUIInputWindowController> self,
-                                                                            SEL _cmd,
-                                                                            UIInputWindowNotificationMode mode,
-                                                                            id info /* UIInputViewSetNotificationInfo */)
-{
-    if (mode == UIInputWindowNotificationModeMoving && ! [self isUndocked]) {
-        (*s_UIInputWindowController__postEndNotifications_withInfo_Imp)(self, _cmd, UIInputWindowNotificationModeUndocking, info);
-    }
-    else {
-        (*s_UIInputWindowController__postEndNotifications_withInfo_Imp)(self, _cmd, mode, info);
-    }
-}
-
-@interface UIInputWindowControllerFixes : NSObject
-
-@end
-
-@implementation UIInputWindowControllerFixes
-
-+ (void)load
-{
-    // TODO: Bug at least on iOS 8 and iOS 8.1. Will hopefully be fixed in 8.2, extend version range if not the case
-    if (NSFoundationVersionNumber > NSFoundationVersionNumber_iOS_7_1 && NSFoundationVersionNumber <= 1141.100000 /* iOS 8.1 */) {
-        s_UIInputWindowController__postStartNotifications_withInfo_Imp = (void (*)(id, SEL, UIInputWindowNotificationMode, id))hls_class_swizzleSelector(NSClassFromString(@"UIInputWindowController"),
-                                                                                                                                                         NSSelectorFromString(@"postStartNotifications:withInfo:"),
-                                                                                                                                                         (IMP)swizzled_UIInputWindowController__postStartNotifications_withInfo);
-        s_UIInputWindowController__postEndNotifications_withInfo_Imp = (void (*)(id, SEL, UIInputWindowNotificationMode, id))hls_class_swizzleSelector(NSClassFromString(@"UIInputWindowController"),
-                                                                                                                                                       NSSelectorFromString(@"postEndNotifications:withInfo:"),
-                                                                                                                                                       (IMP)swizzled_UIInputWindowController__postEndNotifications_withInfo);
-    }
-}
-
-@end
