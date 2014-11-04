e8dfd33e646589ff1f3099fcf22530cde9c882c4
Samuel Défago <defagos@gmail.com>
2014-11-04 11:00:46 +0100

    Disable recursive bindings for UIImageView

---
+++ CoconutKit/Sources/Bindings/UIImageView+HLSViewBinding.h
@@ -0,0 +1,16 @@
+//
+//  UIImageView+HLSViewBinding.h
+//  CoconutKit
+//
+//  Created by Samuel Défago on 04.11.14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import "UIView+HLSViewBindingImplementation.h"
+
+/**
+ * Private category implementing binding behavior for UIWebView
+ */
+@interface UIImageView (HLSViewBinding) <HLSViewBindingImplementation>
+
+@end
---
+++ CoconutKit/Sources/Bindings/UIImageView+HLSViewBinding.m
@@ -0,0 +1,20 @@
+//
+//  UIImageView+HLSViewBinding.m
+//  CoconutKit
+//
+//  Created by Samuel Défago on 04.11.14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import "UIImageView+HLSViewBinding.h"
+
+@implementation UIImageView (HLSViewBinding)
+
+#pragma mark HLSViewBindingImplementation protocol implementation
+
+- (BOOL)bindsSubviewsRecursively
+{
+    return NO;
+}
+
+@end
