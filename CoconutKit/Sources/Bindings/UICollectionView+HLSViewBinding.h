81109fa5a6dd815daf3ec622f33ea302ecdd15cd
Samuel Défago <defagos@gmail.com>
2014-11-04 16:47:02 +0100

    Disable recursive bindings at collection view boundaries

---
+++ CoconutKit/Sources/Bindings/UICollectionView+HLSViewBinding.h
@@ -0,0 +1,13 @@
+//
+//  UICollectionView+HLSViewBinding.h
+//  CoconutKit
+//
+//  Created by Samuel Défago on 04.11.14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import "UIView+HLSViewBindingImplementation.h"
+
+@interface UICollectionView (HLSViewBinding) <HLSViewBindingImplementation>
+
+@end
