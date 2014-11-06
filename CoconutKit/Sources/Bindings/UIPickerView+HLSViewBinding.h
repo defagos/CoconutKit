521b9c1635f96994cef4c96b8b2904f50fdcb960
Samuel Défago <defagos@gmail.com>
2014-11-06 08:03:35 +0100

    Disable recursive bindings for UIPickerView

---
+++ CoconutKit/Sources/Bindings/UIPickerView+HLSViewBinding.h
@@ -0,0 +1,13 @@
+//
+//  UIPickerView+HLSViewBinding.h
+//  CoconutKit
+//
+//  Created by Samuel Défago on 06.11.14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import "UIView+HLSViewBindingImplementation.h"
+
+@interface UIPickerView (HLSViewBinding)
+
+@end
---
+++ CoconutKit/Sources/Bindings/UIPickerView+HLSViewBinding.m
@@ -0,0 +1,20 @@
+//
+//  UIPickerView+HLSViewBinding.m
+//  CoconutKit
+//
+//  Created by Samuel Défago on 06.11.14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import "UIPickerView+HLSViewBinding.h"
+
+@implementation UIPickerView (HLSViewBinding)
+
+#pragma mark HLSViewBindingImplementation protocol implementation
+
+- (BOOL)bindsSubviewsRecursively
+{
+    return NO;
+}
+
+@end
