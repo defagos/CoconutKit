39ae6342b66a81ff3b660f4515aa08ad4d77c887
Samuel Défago <defagos@gmail.com>
2014-11-06 15:27:15 +0100

    Add TODO

--- CoconutKit/Sources/Bindings/UIView+HLSViewBinding.h
+++ CoconutKit/Sources/Bindings/UIView+HLSViewBinding.h
@@ -94,6 +94,7 @@
  * TODO: Document validation via -validate and soon-to-be-implemented -check methods
  * TODO: Document: Validation is always available, even if a view cannot update the underlying model object
  * TODO: Document that bindings work with primitive types as well (automatic boxing / unboxing)
+ * TODO: Document: Validation only performed when a value gets updated via a bound control
  *
  * Here is how UIKit view classes play with bindings:
  *   - UILabel: The label displays the value which the keypath points at. Bindings are not recursive. The only 
