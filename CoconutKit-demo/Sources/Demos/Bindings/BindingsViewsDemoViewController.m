cbd2de13867f7723fc5268334b015a6f4f236938
Samuel Défago <defagos@gmail.com>
2014-11-10 15:31:01 +0100

    Add demo for binding to a view having a property

--- CoconutKit-demo/Sources/Demos/Bindings/BindingsViewsDemoViewController.m
+++ CoconutKit-demo/Sources/Demos/Bindings/BindingsViewsDemoViewController.m
@@ -11,15 +11,21 @@
 #import "Employee.h"
 #import "EmployeeHeaderView.h"
 #import "EmployeeTableViewCell.h"
+#import "EmployeeView.h"
 
 @interface BindingsViewsDemoViewController ()
 
 @property (nonatomic, strong) NSArray *employees;
+
 @property (nonatomic, weak) IBOutlet UITableView *tableView;
+@property (nonatomic, weak) IBOutlet EmployeeView *employeeView;
 
 @end
 
-@implementation BindingsViewsDemoViewController
+@implementation BindingsViewsDemoViewController {
+@private
+    NSInteger _currentEmployeeIndex;
+}
 
 #pragma mark Object creation and destruction
 
@@ -63,6 +69,8 @@
     
     self.tableView.dataSource = self;
     self.tableView.delegate = self;
+    
+    [self changeEmployee];
 }
 
 #pragma mark Localization
@@ -72,6 +80,14 @@
     [super localize];
     
     self.title = NSLocalizedString(@"Views", nil);
+}
+
+#pragma mark Updating data
+
+- (void)changeEmployee
+{
+    _currentEmployeeIndex = (_currentEmployeeIndex + 1) % [self.employees count];
+    self.employeeView.employee = [self.employees objectAtIndex:_currentEmployeeIndex];
 }
 
 #pragma mark UITableViewDataSource protocol implementation
@@ -98,6 +114,13 @@
 - (UIView *)tableView:(UITableView *)tableView viewForHeaderInSection:(NSInteger)section
 {
     return [EmployeeHeaderView view];
+}
+
+#pragma mark Actions
+
+- (IBAction)changeEmployee:(id)sender
+{
+    [self changeEmployee];
 }
 
 @end
