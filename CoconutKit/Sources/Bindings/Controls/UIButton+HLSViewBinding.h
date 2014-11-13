99f537cddfc94421d765cd959d18f63406ce4196
Samuel Défago <defagos@gmail.com>
2014-11-13 20:54:58 +0100

    Disable recursive bindings for UIButton

---
+++ CoconutKit/Sources/Bindings/Controls/UIButton+HLSViewBinding.h
@@ -0,0 +1,13 @@
+//
+//  UIButton+HLSViewBinding.h
+//  CoconutKit
+//
+//  Created by Samuel Defago on 13/11/14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import "UIView+HLSViewBindingImplementation.h"
+
+@interface UIButton (HLSViewBinding) <HLSViewBindingImplementation>
+
+@end
