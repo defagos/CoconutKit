06b1ec6bb303767063a4f069455e683ffd9ad668
Samuel Défago <defagos@gmail.com>
2014-11-14 16:06:26 +0100

    Create help main layout

--- CoconutKit/Sources/Bindings/Overlay/HLSViewBindingHelpViewController.m
+++ CoconutKit/Sources/Bindings/Overlay/HLSViewBindingHelpViewController.m
@@ -10,6 +10,13 @@
 
 #import "NSBundle+HLSExtensions.h"
 
+@interface HLSViewBindingHelpViewController ()
+
+@property (nonatomic, weak) IBOutlet UIScrollView *scrollView;
+@property (nonatomic, weak) IBOutlet UIView *contentView;
+
+@end
+
 @implementation HLSViewBindingHelpViewController
 
 #pragma mark Object creation and destruction
@@ -17,6 +24,23 @@
 - (instancetype)init
 {
     return [self initWithBundle:[NSBundle coconutKitBundle]];
+}
+
+#pragma mark View lifecycle
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    self.scrollView.contentSize = self.contentView.bounds.size;
+    [self.scrollView addSubview:self.contentView];
+}
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
+    
+    [self.scrollView flashScrollIndicators];
 }
 
 #pragma mark Localization
