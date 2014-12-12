a0e6031230a9333e98ffbed734c733f6fa0364b4
Samuel Défago <defagos@gmail.com>
2014-12-12 12:53:26 +0100

    Add test runner project
    Needed to be able to run tests on a device

---
+++ CoconutKit-tests/Sources/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CoconutKit-tests-runner
+//
+//  Created by Samuel Defago on 12/12/14.
+//  Copyright (c) 2014 Samuel Défago. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        // The app is dummy, we must only ensure it stays alive while loading the test bundle
+        return UIApplicationMain(argc, argv, nil, nil);
+    }
+}
