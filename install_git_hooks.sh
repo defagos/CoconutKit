1ea2094cb88fa32595900cc6c8719dbaa80b9e0d
Samuel Défago <defagos@gmail.com>
2014-12-19 13:48:02 +0100

    Create hook directory if missing

--- install_git_hooks.sh
+++ install_git_hooks.sh
@@ -3,9 +3,15 @@
 SCRIPT_FILE_DIR=`dirname $0`
 SCRIPT_FILE_DIR=`cd $SCRIPT_FILE_DIR; pwd`
 
+HOOKS_GIT_DIR="$SCRIPT_FILE_DIR/.git/hooks"
+
+if [ ! -d $"$HOOKS_GIT_DIR" ]; then
+    mkdir -p "$HOOKS_GIT_DIR"
+fi
+
 for file in "$SCRIPT_FILE_DIR"/git_hooks/*; do
     file_name=`basename $file`
-    destination_file="$SCRIPT_FILE_DIR/.git/hooks/$file_name"
+    destination_file="$HOOKS_GIT_DIR/$file_name"
 
     if [ -f "$destination_file" ]; then
         rm -f "$destination_file"
