d0967c326016a106cb5d60e106e87fae0c88e941
Samuel Défago <defagos@gmail.com>
2015-01-27 07:36:56 +0100

    Fix git hook installation script

--- install_git_hooks.sh
+++ install_git_hooks.sh
@@ -5,7 +5,7 @@ SCRIPT_FILE_DIR=`cd $SCRIPT_FILE_DIR; pwd`
 
 HOOKS_GIT_DIR="$SCRIPT_FILE_DIR/.git/hooks"
 
-if [ ! -d $"$HOOKS_GIT_DIR" ]; then
+if [ ! -d "$HOOKS_GIT_DIR" ]; then
     mkdir -p "$HOOKS_GIT_DIR"
 fi
 
@@ -13,7 +13,7 @@ for file in "$SCRIPT_FILE_DIR"/git_hooks/*; do
     file_name=`basename $file`
     destination_file="$HOOKS_GIT_DIR/$file_name"
 
-    if [ -f "$destination_file" ]; then
+    if [ -L "$destination_file" ]; then
         rm -f "$destination_file"
     fi
     ln -s "$file" "$destination_file"
