c5543d0713a89dfae5d487d6f232fbc76b5f66f5
Samuel Défago <defagos@gmail.com>
2015-02-13 16:06:43 +0100

    Restore unit tests (remove now useless ones)

---
+++ CoconutKit-tests/Sources/Core/NSCalendar+HLSExtensionsTestCase.h
@@ -0,0 +1,10 @@
+//
+//  NSCalendar+HLSExtensionsTestCase.h
+//  CoconutKit-demo
+//
+//  Created by Samuel Défago on 17.08.11.
+//  Copyright 2011 Samuel Défago. All rights reserved.
+//
+
+@interface NSCalendar_HLSExtensionsTestCase : XCTestCase
+@end
