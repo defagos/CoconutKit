1fc5cd0599bf1460f960899a4b4d38e7050e4265
Samuel Défago <defagos@gmail.com>
2015-03-11 18:59:15 +0100

    Add same super method call requirements as HLSViewController

--- CoconutKit/Sources/ViewControllers/HLSCollectionViewController.h
+++ CoconutKit/Sources/ViewControllers/HLSCollectionViewController.h
@@ -19,3 +19,24 @@
 - (instancetype)initWithBundle:(NSBundle *)bundle NS_REQUIRES_SUPER;
 
 @end
+
+@interface UICollectionViewController (HLSRequiresSuper)
+
+- (instancetype)initWithNibName:(NSString *)nibName bundle:(NSBundle *)bundle NS_REQUIRES_SUPER;
+- (instancetype)initWithCoder:(NSCoder *)aDecoder NS_REQUIRES_SUPER;
+- (void)viewDidLoad NS_REQUIRES_SUPER;
+- (void)viewWillAppear:(BOOL)animated NS_REQUIRES_SUPER;
+- (void)viewDidAppear:(BOOL)animated NS_REQUIRES_SUPER;
+- (void)viewWillDisappear:(BOOL)animated NS_REQUIRES_SUPER;
+- (void)viewDidDisappear:(BOOL)animated NS_REQUIRES_SUPER;
+- (void)viewWillUnload NS_REQUIRES_SUPER;
+- (void)viewDidUnload NS_REQUIRES_SUPER;
+- (void)willRotateToInterfaceOrientation:(UIInterfaceOrientation)toInterfaceOrientation duration:(NSTimeInterval)duration NS_REQUIRES_SUPER;
+- (void)didRotateFromInterfaceOrientation:(UIInterfaceOrientation)fromInterfaceOrientation NS_REQUIRES_SUPER;
+- (void)didReceiveMemoryWarning NS_REQUIRES_SUPER;
+- (void)willMoveToParentViewController:(UIViewController *)parent NS_REQUIRES_SUPER;
+- (void)didMoveToParentViewController:(UIViewController *)parent NS_REQUIRES_SUPER;
+- (BOOL)shouldAutorotate NS_REQUIRES_SUPER;
+- (NSUInteger)supportedInterfaceOrientations NS_REQUIRES_SUPER;
+
+@end
