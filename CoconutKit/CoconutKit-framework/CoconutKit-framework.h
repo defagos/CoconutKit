a83838e004e402c7064dd0233d8e92daa2086edb
Samuel Défago <defagos@gmail.com>
2015-03-20 19:38:30 +0100

    Add Cocoa Touch framework target

---
+++ CoconutKit/CoconutKit-framework/CoconutKit-framework.h
@@ -0,0 +1,15 @@
+//
+//  Copyright (c) Samuel Défago. All rights reserved.
+//
+//  Licence information is available from the LICENCE file.
+//
+
+//! Project version number for CoconutKit-framework.
+FOUNDATION_EXPORT double CoconutKit_VersionNumber;
+
+//! Project version string for CoconutKit-framework.
+FOUNDATION_EXPORT const unsigned char CoconutKit_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CoconutKit_framework/PublicHeader.h>
+
+
