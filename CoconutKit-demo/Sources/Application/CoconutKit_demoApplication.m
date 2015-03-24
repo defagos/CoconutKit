b9b6ad73c8e3aedb3b5d000c503770019e7a6df8
Samuel Défago <defagos@gmail.com>
2015-03-24 06:53:07 +0100

    Use non-deprecated iOS 7 style

--- CoconutKit-demo/Sources/Application/CoconutKit_demoApplication.m
+++ CoconutKit-demo/Sources/Application/CoconutKit_demoApplication.m
@@ -89,15 +89,15 @@
             self.rootViewController = navigationController;
             
             UIBarButtonItem *languageBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Language", nil)
-                                                                                       style:UIBarButtonItemStyleBordered 
+                                                                                       style:UIBarButtonItemStylePlain
                                                                                       target:self 
                                                                                       action:@selector(toggleLanguageSheet:)];
             UIBarButtonItem *logsButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Log", nil)
-                                                                               style:UIBarButtonItemStyleBordered
+                                                                               style:UIBarButtonItemStylePlain
                                                                               target:self
                                                                               action:@selector(showSettings:)];
             UIBarButtonItem *debugOverlayBarButtonItem = [[UIBarButtonItem alloc] initWithTitle:NSLocalizedString(@"Bindings", nil)
-                                                                                          style:UIBarButtonItemStyleBordered
+                                                                                          style:UIBarButtonItemStylePlain
                                                                                          target:self
                                                                                          action:@selector(showBindingDebugOverlay:)];
             demosListViewController.navigationItem.rightBarButtonItems = @[languageBarButtonItem, logsButtonItem, debugOverlayBarButtonItem];
